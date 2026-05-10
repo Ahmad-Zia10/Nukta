c673cd3a1d922c71fc94dec1a4cadea5ae3c264a
Ahmad-Zia10 <mail.ahmadzia07@gmail.com>
2026-05-10 23:47:42 +0530

    increase file upload size limit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -23,8 +23,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Body parsing middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit : '10mb'}));
+app.use(express.urlencoded({ extended: true, limit : '10mb' }));
 
 // Cookie parser
 app.use(cookieParser());
